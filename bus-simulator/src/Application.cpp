931176aacd2a7b09bc0d078a391c8dc398a27414
kasiabadio <kasiabadio@hotmail.com>
2021-07-27 08:04:33 +0200

    Created Scene class

--- bus-simulator/src/Application.cpp
+++ bus-simulator/src/Application.cpp
@@ -1,12 +1,53 @@
 
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
-#include <iostream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "Scene.h"
+
+
+void error_callback(int error, const char* description) {
+	fputs(description, stderr);
+}
+
+
 int main(void)
 {
+    
+  
+	glfwSetErrorCallback(error_callback);
+
+	if (!glfwInit())
+		return -1;
+	
+    Scene scene(500, 500);
+	glfwMakeContextCurrent(scene.read_window());
+	glfwSwapInterval(1);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Error: glewInit()" << std::endl;
+	}
+
+   
+	
+	while(!glfwWindowShouldClose(scene.read_window()))
+	{
+		scene.draw_scene();
+		glfwPollEvents();
+	}
+
+
+    glfwTerminate();
     return 0;
 }
