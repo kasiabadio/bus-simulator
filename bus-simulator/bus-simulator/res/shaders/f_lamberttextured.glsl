e41da24fb106dfbb1828a1cbc0218e708a10c3b0
kasiabadio <kasiabadio@hotmail.com>
2021-09-24 11:49:37 +0200

    Two light sources

--- bus-simulator/bus-simulator/res/shaders/f_lamberttextured.glsl
+++ bus-simulator/bus-simulator/res/shaders/f_lamberttextured.glsl
@@ -6,15 +6,23 @@ out vec4 pixel_color;
 
 //Zmienne interpolowane
 in vec2 i_tc;
-in float i_nl;
-in float i_rv;
+in float i_nl[2];
+in float i_rv[2];
 
 
 void main(void) {
     vec4 color=texture(tex, i_tc);
+
 	if (color.a < 0.1)
 		discard;
+
 	// ls*ks*rv = vec4(1,1,1,0)*vec4(1,1,1,0)*i_rv
-	pixel_color=vec4(color.rgb*i_nl,color.a)+vec4(i_rv,i_rv,i_rv,0);
-	//pixel_color=vec4(color.rgb*i_nl,color.a);
+
+	pixel_color = vec4(0,0,0,0);
+	for (int i = 0; i < 2; i++)
+	{
+		pixel_color+=vec4(color.rgb*i_nl[i],color.a)+vec4(i_rv[i],i_rv[i],i_rv[i],0);
+		
+		//pixel_color=vec4(color.rgb*i_nl_first,color.a);
+	}
 }
--- bus-simulator/bus-simulator/res/shaders/v_lamberttextured.glsl
+++ bus-simulator/bus-simulator/res/shaders/v_lamberttextured.glsl
@@ -6,6 +6,7 @@ uniform mat4 V;
 uniform mat4 M;
 
 uniform vec4 light_dir_first = vec4(-5,10,-5,0);
+uniform vec4 light_dir_second = vec4(50,5,2,0);
 
 layout (location=0) in vec4 vertex; 
 layout (location=1) in vec4 normal; 
@@ -13,30 +14,34 @@ layout (location=2) in vec2 texCoord;
 
 
 out vec2 i_tc;
-out float i_nl;
-out float i_rv;
+out float i_nl[2];
+out float i_rv[2];
 
 void main(void) {
     
     gl_Position=P*V*M*vertex;
 
     vec4 n = normalize(V*M*normal);
-    vec4 l = normalize(V*light_dir_first - V*M*vertex);
+    vec4 l_first = normalize(V*light_dir_first - V*M*vertex); //lightDir
+    vec4 l_second = normalize(V*light_dir_second - V*M*vertex);
+
     // znormalizowany wektor r w przestrzeni oka
-    vec4 r = reflect(-l, n);
+    vec4 r_first = reflect(-l_first, n); //reflectDir
+    vec4 r_second = reflect(-l_second, n); //reflectDir
     
     // znormalizowany wektor v w przestrzeni oka
-
     /*vertex jest w przestrzeni modelu dlatego mno¿ymy
     zeby przeniesc z przestrzeni modelu do œwiata i potem razy macierz widoku
     zeby przeniesc z przestrzeni swiata do przestrzeni oka */
 
-    vec4 v = normalize(vec4(0,0,0,1) - V*M*vertex);
+    vec4 v = normalize(vec4(0,0,0,1) - V*M*vertex); 
 
     // test czy mieœci siê w przedziale dwóch argumentów
-    i_nl = clamp(dot(n, l), 0, 1);
-    
-    i_rv = pow(clamp(dot(r, v), 0, 1), 40);
+    i_nl[0] = clamp(dot(n, l_first), 0, 1);
+    i_nl[1] = clamp(dot(n, l_second), 0, 1);
+
+    i_rv[0] = pow(clamp(dot(r_first, v), 0, 1), 40); 
+    i_rv[1] = pow(clamp(dot(r_second, v), 0, 1), 100); 
 
     i_tc = texCoord;
 }
